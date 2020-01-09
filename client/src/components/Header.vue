0cbbbba27b0dabae77aad695d096c2ed6734d5f6
Matthias <dunkelberg.matthias@yahoo.de>
2020-01-09 14:52:46 +0100

    @media for headero

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="Header">
-    <router-link to="/">
        <div class="header">
           <h1>Solar Logger</h1>          
        </div> 
-    </router-link>
   </div>
 </template>
 
@@ -16,15 +14,20 @@
 
 <style lang="scss">
   @import "../assets/styles/_variables.scss";
-  
+
 .header {
   @include container();  
   font-family: Tahoma, Geneva, sans-serif;
-  padding: 10px;  
+  padding: rem(1);
   text-align: center;
   background: $header-backg-color;
   color: white;
-  font-size: 20px;
+  font-size: rem(17);
+}
+@media (max-width: 600px) {
+  .header {
+    font-size: rem(10);    
+  }
 }
 
 a:hover, a:visited, a:link, a:active
